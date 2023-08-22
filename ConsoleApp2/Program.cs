bdc88ab3352a0f1c7da8884f810d23fdffe5dfd3
neona <neona@DESKTOP-CJQVRDG>
2023-08-22 18:49:53 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "vardas", surname = "pavarde", work = "software developer", email = "email@email.com", phone = "+123456789";
+int age = 25;
+Console.WriteLine("=========VIZITINE=========");
+Console.WriteLine("Vardas: " +name+ " " +surname);
+Console.WriteLine("Amzius: " +age);
+Console.WriteLine("Pareigos: " +work);
+Console.WriteLine("E. Pastas: " +email);
+Console.WriteLine("telefonas: " +phone);
+Console.WriteLine("===========================");
